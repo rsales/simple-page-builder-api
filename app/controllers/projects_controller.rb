afb60872f71a1046ef91402d1dd01c4451c30692
Rafael Sales <rafael.sales93@hotmail.com>
2026-09-19 19:51:20 -0300

    feat: replicate projects API

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,16 +5,38 @@ class ProjectsController < ApplicationController
     render json: projects
   end
 
-  def create
-    puts "PARAMS CLASS: #{params.class}"
-    puts "PARAMS: #{params.inspect}"
+  def show
+    project = Project.find(params[:id])
 
+    render json: project
+  end
+
+  def create
     project = Project.create!(project_params)
 
     render json: {
       message: "Projeto criado com sucesso!",
       project: project
     }, status: :created
+  end
+
+  def update
+    project = Project.find(params[:id])
+    project.update!(project_params)
+
+    render json: {
+      message: "Projeto atualizado com sucesso!",
+      project: project
+    }
+  end
+
+  def destroy
+    project = Project.find(params[:id])
+    project.destroy!
+
+    render json: {
+      message: "Projeto excluído com sucesso!"
+    }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   scope "/api" do
-    resources :projects, only: [ :index, :create ]
+    resources :projects, only: [ :index, :show, :create, :update, :destroy ]
   end
 end
